70f2b3763ccbbf447f742b78f77aa96b5b04662b
anthony <anthony4@ualberta.ca>
2021-05-10 15:09:26 -0600

    Updates

---
+++ js/playButton.js
@@ -0,0 +1,24 @@
+$("#play-video").on("click", function (e) {
+    e.preventDefault();
+    $("#video-overlay").addClass("open");
+    $("#video-overlay").append(
+        '<iframe width="560" height="315" src="images/Phenocount Promo Video.mp4" frameborder="0" allowfullscreen></iframe>'
+
+    );
+  });
+  
+  $(".video-overlay, .video-overlay-close").on("click", function (e) {
+    e.preventDefault();
+    close_video();
+  });
+  
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      close_video();
+    }
+  });
+  
+  function close_video() {
+    $(".video-overlay.open").removeClass("open").find("iframe").remove();
+  }
+  
